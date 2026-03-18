ac276ebf8ed6a6043be28e51ab22765bf5b4f18c
Pedro Silva <pedrohsesilva@gmail.com>
2026-03-17 22:17:26 -0300

    feat: update layout metadata and language, add icon image, and configure Snyk settings

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
+      { protocol: 'https', hostname: 'github.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
     ],
   },
 };
-
 export default nextConfig;
